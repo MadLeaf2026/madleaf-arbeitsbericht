d6ef6f387f54a9c153c221ec2cb55f1912810003
MadLeaf2026 <giuseppe.maddalena@madleaf.de>
2026-07-09 21:36:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,18 @@
-const CACHE = 'madleaf-v3';
+const CACHE = 'madleaf-v4';
 const BASE = '/madleaf-arbeitsbericht/';
 const ASSETS = [BASE, BASE+'index.html', BASE+'manifest.json', BASE+'logo.png', BASE+'Signature_giuseppe.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
@@ -21,7 +24,7 @@ self.addEventListener('fetch', e => {
         const clone = res.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
         return res;
-      }).catch(() => caches.match(BASE+'index.html'));
+      }).catch(() => caches.match(BASE + 'index.html'));
     })
   );
 });
